28326e1ee6490717013e101913c48e8f679ab424
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-09-29 20:40:16 +0800

    basic HTML added to index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// var with letters of the alphabet
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+
+// computer selects letter at random and stores in a var
+var compChoice = alphabet.charAt(Math.random() * 26)
+console.log(compChoice)
+
+
+// var keeping track of letters that player has guessed
+    // key press adds letter to var
+    // can't guess the same letter twice
+
+
+    // var with number of guesses allowed
+    // after each valid user guess this var decreases by 1
+
+
+    // var counting player wins
+
+
+    // var counting player losses
