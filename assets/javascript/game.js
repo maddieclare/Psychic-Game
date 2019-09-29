4de0d75b97dfb8a80092a510be47d4a63d23c967
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-09-29 21:31:13 +0800

    Added variables for wins, losses, and no. of guesses left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,22 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 // array keeping track of letters that player has guessed
 var lettersGuessed = [];
 
+// var with number of guesses allowed
+var guessesLeft = 12;
+
+// var counting player wins
+var playerWins = 0;
+
+// var counting player losses
+var playerLosses = 0;
+
 // computer selects letter at random and stores in a var
 var compChoice = alphabet.charAt(Math.random() * 26);
 console.log(compChoice);
 
 // function allowing player to guess any letter of the alphabet by pressing the corresponding key
 document.onkeyup = function(selection) {
+  // key press adds letter to var
   var userGuess = selection.key.toLowerCase();
   console.log(userGuess);
 
@@ -17,21 +27,17 @@ document.onkeyup = function(selection) {
   if (alphabet.search(userGuess) == -1) {
     alert("Please guess a valid letter of the alphabet.");
     return;
+  // can't guess the same letter twice
   } else if (lettersGuessed.indexOf(userGuess) !== -1) {
     alert("You've already guessed that letter!");
     return;
   } else {
-      lettersGuessed.push(userGuess);
-      console.log(lettersGuessed);
+    lettersGuessed.push(userGuess);
+    console.log(lettersGuessed);
   }
 };
 
-// key press adds letter to var
-// can't guess the same letter twice
 
-// var with number of guesses allowed
-// after each valid user guess this var decreases by 1
+// after each valid user guess, lettersGuessed var decreases by 1
 
-// var counting player wins
-
-// var counting player losses
+// if lettersGuessed var reaches 0, player loses
