280419549ac708add948f64329f0ec1aeee76d36
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-09-29 21:10:47 +0800

    Player can select a letter of the alphabet using onkeyup event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,29 @@
 // var with letters of the alphabet
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-
 // computer selects letter at random and stores in a var
 var compChoice = alphabet.charAt(Math.random() * 26);
 console.log(compChoice);
 
+// function allowing player to guess any letter of the alphabet by pressing the corresponding key
+document.onkeyup = function(selection) {
+  var userGuess = selection.key.toLowerCase();
+  console.log(userGuess);
+
+  // function preventing any non-alphabetical key from being counted as a guess
+  if (alphabet.search(userGuess) == -1) {
+    alert("Please guess a valid letter of the alphabet.");
+    // & don't add this to the guessesLeft or lettersGuessed variables
+  }
+};
 
 // var keeping track of letters that player has guessed
-    // key press adds letter to var
-    // can't guess the same letter twice
+// key press adds letter to var
+// can't guess the same letter twice
 
+// var with number of guesses allowed
+// after each valid user guess this var decreases by 1
 
-    // var with number of guesses allowed
-    // after each valid user guess this var decreases by 1
+// var counting player wins
 
-
-    // var counting player wins
-
-
-    // var counting player losses
+// var counting player losses
