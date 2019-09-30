02ce1f0e4e9c04687efce7386f610e70ca3a77ba
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-09-30 14:15:39 +0800

    Code working again - defined variables outside of functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
-// var containing letters of the alphabet
+// variable containing all letters of the alphabet
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-// array keeping track of letters that player has guessed
+// array tracking letters player has previously guessed
 var lettersGuessed = [];
 
-// var with number of guesses allowed
+// variable tracking number of player guesses remaining
 var guessesLeft = 9;
 
-// var counting player wins
+// variable tracking player wins
 var playerWins = 0;
 
-// var counting player losses
+// variable tracking player losses
 var playerLosses = 0;
 
 // for stats display via HTML
@@ -24,71 +24,68 @@ function playerStats() {
     "Your guesses so far: " + lettersGuessed;
 }
 
-// computer selects letter at random and stores in compChoice variable
-function compSelect() {
-  var compChoice = alphabet.charAt(Math.random() * 26);
-  console.log("Computer's selection: " + compChoice);
-}
-
-// function preventing player from guessing the same letter twice
-function doubleGuess() {
-  alert("You've already guessed that letter!");
-  return;
-}
-
-// function preventing any non-alphabetical key from being counted as a guess
-function notLetter() {
-  alert("Please guess a valid letter of the alphabet.");
-  return;
-}
-
-// pushes valid player guesses to lettersGuessed array
-function letterTrack() {
-  lettersGuessed.push(userGuess);
-  console.log("Letters guessed: " + lettersGuessed);
-}
-
-// after each valid user guess, guessesLeft variable decreases by 1
-function guessCounter() {
-  guessesLeft -= 1;
-  console.log("Guesses left: " + guessesLeft);
-}
-
-// player lose condition
-function playerLoss() {
-  alert("You lose :(");
-  playerLosses += 1;
-  lettersGuessed = [];
-  guessesLeft = 9;
-  console.log(playerLosses);
-}
-
-// player win condition
-function playerWin() {
-  alert("You win!");
-  playerWins += 1;
-  lettersGuessed = [];
-  guessesLeft = 9;
-  console.log(playerWins);
-}
-
-compSelect();
+// selects character at random from alphabet variable
+var compChoice = alphabet.charAt(Math.random() * 26);
+console.log("Computer's selection: " + compChoice);
 
 // player can guess any letter of the alphabet by pressing the corresponding key
 document.onkeyup = function(selection) {
-  // key press adds letter to var
+  // key press adds letter to variable
   var userGuess = selection.key.toLowerCase();
   console.log("Player guess: " + userGuess);
-  
-  if (alphabet.search(userGuess) == -1) {
-    doubleGuess();
-  } else if (lettersGuessed.indexOf(userGuess) !== -1) {
+
+  // prevents player from guessing the same letter twice
+  function doubleGuess() {
+    alert("You've already guessed that letter!");
+  }
+
+  // prevents any non-alphabetical key from being counted as a guess
+  function notLetter() {
+    alert("Please choose a valid letter.");
+  }
+
+  // pushes valid player guesses to lettersGuessed array
+  function letterTrack() {
+    lettersGuessed.push(userGuess);
+    console.log("Letters guessed: " + lettersGuessed);
+  }
+
+  // decreases guessesLeft variable by 1 after each valid (incorrect) user guess
+  function guessCounter() {
+    guessesLeft -= 1;
+    console.log("Guesses left: " + guessesLeft);
+  }
+
+  // player lose condition
+  function playerLoss() {
+    alert("You lose :(");
+    playerLosses += 1;
+    lettersGuessed = [];
+    guessesLeft = 9;
+    console.log("Player losses: " + playerLosses);
+  }
+
+  // player win condition
+  function playerWin() {
+    alert("You win!");
+    playerWins += 1;
+    lettersGuessed = [];
+    guessesLeft = 9;
+    console.log("Player wins: " + playerWins);
+  }
+
+  if (alphabet.search(userGuess) === -1) {
     notLetter();
+    guessCounter();
+  } else if (lettersGuessed.indexOf(userGuess) !== -1) {
+    doubleGuess();
+    guessCounter();
   } else if (userGuess == compChoice) {
     playerWin();
-  } else if (guessesLeft == 0) {
+  } else if (guessesLeft == 1) {
     playerLoss();
   } else {
     letterTrack();
+    guessCounter();
   }
 };
