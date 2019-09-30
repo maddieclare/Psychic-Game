941504dc186a2d6967a724a4b21c1921c745b9bf
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-09-30 13:30:43 +0800

    Tidied up code using functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// var with letters of the alphabet
+// var containing letters of the alphabet
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 // array keeping track of letters that player has guessed
@@ -13,44 +13,82 @@ var playerWins = 0;
 // var counting player losses
 var playerLosses = 0;
 
-// after each valid user guess, guessesLeft var decreases by 1
+// for stats display via HTML
+function playerStats() {
+  document.getElementById("playerWins").textContent = "Wins: " + playerWins;
+  document.getElementById("playerLosses").textContent =
+    "Losses: " + playerLosses;
+  document.getElementById("guessesLeft").textContent =
+    "Guesses left: " + guessesLeft;
+  document.getElementById("lettersGuessed").textContent =
+    "Your guesses so far: " + lettersGuessed;
+}
+
+// computer selects letter at random and stores in compChoice variable
+function compSelect() {
+  var compChoice = alphabet.charAt(Math.random() * 26);
+  console.log("Computer's selection: " + compChoice);
+}
+
+// function preventing player from guessing the same letter twice
+function doubleGuess() {
+  alert("You've already guessed that letter!");
+  return;
+}
+
+// function preventing any non-alphabetical key from being counted as a guess
+function notLetter() {
+  alert("Please guess a valid letter of the alphabet.");
+  return;
+}
+
+// pushes valid player guesses to lettersGuessed array
+function letterTrack() {
+  lettersGuessed.push(userGuess);
+  console.log("Letters guessed: " + lettersGuessed);
+}
+
+// after each valid user guess, guessesLeft variable decreases by 1
 function guessCounter() {
   guessesLeft -= 1;
   console.log("Guesses left: " + guessesLeft);
 }
 
-// computer selects letter at random and stores in a var
-var compChoice = alphabet.charAt(Math.random() * 26);
-console.log("Computer's selection: " + compChoice);
+// player lose condition
+function playerLoss() {
+  alert("You lose :(");
+  playerLosses += 1;
+  lettersGuessed = [];
+  guessesLeft = 9;
+  console.log(playerLosses);
+}
 
-// function allowing player to guess any letter of the alphabet by pressing the corresponding key
+// player win condition
+function playerWin() {
+  alert("You win!");
+  playerWins += 1;
+  lettersGuessed = [];
+  guessesLeft = 9;
+  console.log(playerWins);
+}
+
+compSelect();
+
+// player can guess any letter of the alphabet by pressing the corresponding key
 document.onkeyup = function(selection) {
   // key press adds letter to var
   var userGuess = selection.key.toLowerCase();
   console.log("Player guess: " + userGuess);
-
-  // function preventing any non-alphabetical key from being counted as a guess
+  
   if (alphabet.search(userGuess) == -1) {
-    alert("Please guess a valid letter of the alphabet.");
-    return;
-    // can't guess the same letter twice
+    doubleGuess();
   } else if (lettersGuessed.indexOf(userGuess) !== -1) {
-    alert("You've already guessed that letter!");
-    return;
+    notLetter();
   } else if (userGuess == compChoice) {
-    alert("You win!");
-    playerWins += 1;
-    lettersGuessed = [];
-    guessesLeft = 9;
-    console.log(playerWins);
-    return;
+    playerWin();
+  } else if (guessesLeft == 0) {
+    playerLoss();
   } else {
-    lettersGuessed.push(userGuess);
-    console.log("Letters guessed: " + lettersGuessed);
-    guessCounter();
+    letterTrack();
   }
 };
-
-// if user guess matches computer selection, alert appears and player win count increases by 1; lettersGuessed & guessesLeft vars reset
-
-// if lettersGuessed var reaches 0, player loss count increases by 1; lettersGuessed & guessesLeft vars reset
